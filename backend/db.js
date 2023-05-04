1df40539ba220003fff6bc964cbddf8cd0eb90a6
mohit jangid <mohitjangid183@gmail.com>
2023-05-04 16:33:29 +0530

    add connection string

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/inotebook?directConnection=true"
+// const mongoURI = "mongodb://localhost:27017/inotebook?directConnection=true"
+const mongoURI = "mongodb+srv://root:root@cluster0.fuef4me.mongodb.net/inotebook"
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=> {
--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -21,3 +21,4 @@ const Noteitem = (props) => {
 }
 
 export default Noteitem
+
